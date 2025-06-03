b1911403aabaee7f5dd5c835822c4ff2681433bf
Kübra Dönmez <kubradonmezz@outlook.com.tr>
2025-06-03 23:25:34 +0300

    [JA-4] - Inheritance (Kalıtım)

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Car myCar = new Car();
 
-        // set metoduyla değer atama
-        myCar.setBrand("Honda");
-        myCar.setModel("Civic");
-        myCar.setYear(2022);
+        myCar.setSpeed(120);  // speed değerini ata
 
-        // get metoduyla ekrana yazdırma
-        System.out.println("Marka: " + myCar.getBrand());
-        System.out.println("Model: " + myCar.getModel());
-        System.out.println("Yıl: " + myCar.getYear());
+        System.out.println("Arabanın hızı: " + myCar.getSpeed() + " km/s");
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle {
+    protected int speed;  // protected, alt sınıflardan erişilebilir
+
+    // speed için getter ve setter
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
